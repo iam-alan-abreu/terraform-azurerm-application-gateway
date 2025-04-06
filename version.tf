8d106247f74f300b7dde7554aaabd355593af55d
Alan Abreu <aas2512@gmail.com>
2025-04-06 13:10:00 -0300

    Update version.tf

--- version.tf
+++ version.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.59.0, <= 4.00"
+      version = ">= 04.00, <= 5.00"
     }
   }
   required_version = ">= 0.13"
